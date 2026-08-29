3e2809a4acf2291058ca86f3ffe4b8407ba02e4e
Konstantin <justpack.it@yandex.ru>
2026-08-30 03:14:51 +0700

    edit 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
+// Cases Swiper
 const swiperCases = new Swiper('.cases__wrapper', {
     direction: 'horizontal',
     loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
     pagination: {
         el: '.cases__slider-dots',
         clickable: true,
@@ -9,57 +12,28 @@ const swiperCases = new Swiper('.cases__wrapper', {
         nextEl: '.cases__slider-nav__item--next',
         prevEl: '.cases__slider-nav__item--prev',
     },
-    mousewheel: {
-        enabled: true,
-        thresholdTime: 500,
-        forceToAxis: true
-    },
     breakpoints: {
         480: {
-            slidesPerView: 1,
+            slidesPerView: 1.1,
+            spaceBetween: 16,
         },
         768: {
             slidesPerView: 2,
+            spaceBetween: 20,
         },
         1024: {
             slidesPerView: 3,
+            spaceBetween: 24,
         },
     },
 });
 
-const swiperReviews = new Swiper('.reviews__wrapper', {
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-        el: '.reviews__slider-dots',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.reviews__slider-nav__item--next',
-        prevEl: '.reviews__slider-nav__item--prev',
-    },
-    mousewheel: {
-        enabled: true,
-        thresholdTime: 500,
-        forceToAxis: true
-    },
-    breakpoints: {
-        480: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        1024: {
-            slidesPerView: 3,
-        },
-    },
-});
-
-
+// Articles Swiper
 const swiperArticles = new Swiper('.articles__wrapper', {
     direction: 'horizontal',
     loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
     pagination: {
         el: '.articles__wrapper-dots',
         clickable: true,
@@ -68,20 +42,70 @@ const swiperArticles = new Swiper('.articles__wrapper', {
         nextEl: '.articles__wrapper-nav__item--next',
         prevEl: '.articles__wrapper-nav__item--prev',
     },
-    mousewheel: {
-        enabled: true,
-        thresholdTime: 500,
-        forceToAxis: true
-    },
     breakpoints: {
         480: {
-            slidesPerView: 1,
+            slidesPerView: 1.1,
+            spaceBetween: 16,
         },
         768: {
             slidesPerView: 2,
+            spaceBetween: 20,
         },
         1024: {
             slidesPerView: 3,
+            spaceBetween: 24,
         },
     },
+});
+
+// Reviews Swiper
+const swiperReviews = new Swiper('.reviews__wrapper', {
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    pagination: {
+        el: '.reviews__slider-dots',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.reviews__slider-nav__item--next',
+        prevEl: '.reviews__slider-nav__item--prev',
+    },
+    breakpoints: {
+        480: {
+            slidesPerView: 1.1,
+            spaceBetween: 16,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+        },
+    },
+});
+
+// FAQ Accordion - Close other items on open
+document.querySelectorAll('.faq__item').forEach((item) => {
+    item.addEventListener('toggle', (e) => {
+        if (item.open) {
+            document.querySelectorAll('.faq__item').forEach((otherItem) => {
+                if (otherItem !== item && otherItem.open) {
+                    otherItem.open = false;
+                }
+            });
+        }
+    });
+});
+
+// CTA Form Segmented Method Switcher
+const methodLabels = document.querySelectorAll('.cta__form-card-method__label');
+methodLabels.forEach((label) => {
+    label.addEventListener('click', () => {
+        methodLabels.forEach((l) => l.classList.remove('cta__form-card-method__label--active'));
+        label.classList.add('cta__form-card-method__label--active');
+    });
 });
